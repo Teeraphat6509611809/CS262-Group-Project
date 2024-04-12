8ff08a0dafdc6116bf14d566867923c831d7d400
teeraphat <teeraphat.sir@dome.tu.ac.th>
2024-04-12 17:51:30 +0700

    update auto wd 10 subj

--- protractor/cyt-codeTesting/spec.js
+++ protractor/cyt-codeTesting/spec.js
@@ -1,52 +1,52 @@
 // spec.js
 describe('Protractor Demo App',  function() {
-    // it('should have a title',   function() {
-    //   browser.ignoreSynchronization = true;
-    //   browser.get('http://localhost:5500/');
-    //   expect(browser.getTitle()).toEqual('คำร้องเพิ่ม-ถอน รายวิชา');
-    // });
-    // it('should warn after add more than 10 subjects (Exclusion)' , async function(){
-    //   browser.ignoreSynchronization = true;
-    //   browser.get('http://localhost:5500/');
-    //   for(let i = 0 ; i < 10 ; i++){
-    //     await element(by.id("add-course")).click();
-    //   }
-    //   var EC = protractor.ExpectedConditions;
+    it('should have a title',   function() {
+      browser.ignoreSynchronization = true;
+      browser.get('http://localhost:5500/');
+      expect(browser.getTitle()).toEqual('คำร้องเพิ่ม-ถอน รายวิชา');
+    });
+    it('should warn after add more than 10 subjects (Exclusion)' , async function(){
+      browser.ignoreSynchronization = true;
+      browser.get('http://localhost:5500/');
+      for(let i = 0 ; i < 10 ; i++){
+        await element(by.id("add-course")).click();
+      }
+      var EC = protractor.ExpectedConditions;
     
-    //  await element(by.id("withdraw-course")).click();
-    //   browser.wait(EC.not(EC.alertIsPresent()), 1500, "it's not excluding limit of withdraw course from add course"); // should not alert at 10 add and 1 withdraw
-    //   await element(by.id("add-course")).click();
-    //   var alertDialog = browser.switchTo().alert();
-    //   alertDialog.getText().then(function(alertText) {
-    //     // Assert or perform actions based on the text of the prompt dialog
-    //     expect(alertText).toEqual('สามารถใส่ได้ 10 วิชาเท่านั้น');
-    //   });
-    //   alertDialog.accept();
+     await element(by.id("withdraw-course")).click();
+      browser.wait(EC.not(EC.alertIsPresent()), 1500, "it's not excluding limit of withdraw course from add course"); // should not alert at 10 add and 1 withdraw
+      await element(by.id("add-course")).click();
+      var alertDialog = browser.switchTo().alert();
+      alertDialog.getText().then(function(alertText) {
+        // Assert or perform actions based on the text of the prompt dialog
+        expect(alertText).toEqual('สามารถใส่ได้ 10 วิชาเท่านั้น');
+      });
+      alertDialog.accept();
 
-    //   for(let i = 0 ; i < 9 ; i++){
-    //     await element(by.id("withdraw-course")).click();
-    //   }
-    //   await element(by.id("withdraw-course")).click();
-    //   alertDialog.getText().then(function(alertText) {
-    //     // Assert or perform actions based on the text of the prompt dialog
-    //     expect(alertText).toEqual('สามารถใส่ได้ 10 วิชาเท่านั้น');
-    //   });
-    //   alertDialog.accept();
-    // });
+      for(let i = 0 ; i < 9 ; i++){
+        await element(by.id("withdraw-course")).click();
+      }
+      await element(by.id("withdraw-course")).click();
+      alertDialog.getText().then(function(alertText) {
+        // Assert or perform actions based on the text of the prompt dialog
+        expect(alertText).toEqual('สามารถใส่ได้ 10 วิชาเท่านั้น');
+      });
+      alertDialog.accept();
+    });
 
-    // it('should alert after submit incomplete form',async function(){
-    //   browser.ignoreSynchronization = true;
-    //   browser.get('http://localhost:5500/');
-    //   await element(by.id("studentFirstName")).sendKeys("Songsak");
-    //   await element(by.id("submit")).click();
-    //   var EC = protractor.ExpectedConditions;
-    //   browser.wait(EC.alertIsPresent(), 10000, "alert time out");
-    //   var alertDialog = browser.switchTo().alert();
-    //   alertDialog.getText().then(function(alertText) {
-    //     expect(alertText).toEqual('บันทึกรายวิชาไม่สำเร็จ');
-    //   });
-    //   alertDialog.accept();
-    // });
+    it('should alert after submit incomplete form',async function(){
+      browser.ignoreSynchronization = true;
+      browser.get('http://localhost:5500/');
+      await element(by.id("studentFirstName")).sendKeys("Songsak");
+      await element(by.id("submit")).click();
+      var EC = protractor.ExpectedConditions;
+      browser.wait(EC.alertIsPresent(), 10000, "alert time out");
+      var alertDialog = browser.switchTo().alert();
+      alertDialog.getText().then(function(alertText) {
+        expect(alertText).toEqual('บันทึกรายวิชาไม่สำเร็จ');
+      });
+      alertDialog.accept();
+    });
     it('should pass after everything ok',async function(){
       browser.ignoreSynchronization = true;
       browser.get('http://localhost:5500/');
@@ -66,7 +66,7 @@ describe('Protractor Demo App',  function() {
       element(by.id("province")).sendKeys("คำคำ");
       element(by.id("postalCode")).sendKeys("11111");
       element(by.id("cause")).sendKeys("lmao");
-      let addsubjlist = ['ABCD01', 'EFGH02', 'IJKL03', 'MNOP04', 'QRST05', 'UVWX06', 'YZAB07', 'CDEF08', 'GHIJ09', 'KLMN10'];
+      let addsubjlist = ['AB001', 'EF002', 'IJ003', 'MN004', 'QR005', 'UV006', 'YZ007', 'CD008', 'GH009', 'KL010'];
       let addsubjectNamesList = [
         "Mathematics",
         "Physics",
@@ -117,23 +117,86 @@ describe('Protractor Demo App',  function() {
       ];
       for(let i = 1 ; i <= 10 ; i++){
         await element(by.id("add-course")).click().then(function(){
-        element(by.id("AddsubjectCode"+i)).sendKeys(addsubjlist[i-1]);
-        element(by.id("AddsubjectName"+i)).sendKeys(addsubjectNamesList[i-1]);
-        element(by.id("AddsubjectSection"+i)).sendKeys(addsectionCodesList[i-1]);
-        element(by.id("AddsubjectDate"+i)).sendKeys(addsubjDateList[i-1]);
-        element(by.id("AddsubjectCredit"+i)).sendKeys("3");
-        element(by.id("AddsubjectTeacher"+i)).sendKeys(addSubjTeacherList[i-1]);
+        element(by.id(`AddsubjectCode${i}`)).sendKeys(addsubjlist[i-1]);
+        element(by.id(`AddsubjectName${i}`)).sendKeys(addsubjectNamesList[i-1]);
+        element(by.id(`AddsubjectSection${i}`)).sendKeys(addsectionCodesList[i-1]);
+        element(by.id(`AddsubjectDate${i}`)).sendKeys(addsubjDateList[i-1]);
+        element(by.id(`AddsubjectCredit${i}`)).sendKeys("3");
+        element(by.id(`AddsubjectTeacher${i}`)).sendKeys(addSubjTeacherList[i-1]);
       });
       }
       
-      await element(by.id("withdraw-course")).click().then(function(){
-        element(by.id("WithdrawsubjectCode1")).sendKeys("CS368");
-        element(by.id("WithdrawsubjectName1")).sendKeys("Good subj bad teacher");
-        element(by.id("WithdrawsubjectSection1")).sendKeys("810001");
-        element(by.id("WithdrawsubjectDate1")).sendKeys("Mo.");
-        element(by.id("WithdrawsubjectCredit1")).sendKeys("3");
-        element(by.id("WithdrawsubjectTeacher1")).sendKeys("WTF fuck off!");
+      let withdrawsubjectCodeList = [
+        "AB123",
+        "CD456",
+        "EF789",
+        "GH012",
+        "IJ345",
+        "KL678",
+        "MN901",
+        "OP234",
+        "QR567",
+        "ST890"
+      ];
+      let withdrawsubjectnameList = [
+        "Computer Science 2",
+        "Biology 2",
+        "Physics 2",
+        "Literature 2",
+        "Mathematics 2",
+        "Chemistry 2",
+        "History 2",
+        "Economics 2",
+        "Psychology 2",
+        "Sociology 2"
+      ];
+      let withdrawsectionCodesList = [
+        '7654321',
+        '8765432',
+        '9876543',
+        '0987654',
+        '1098765',
+        '2109876',
+        '3210987',
+        '4321098',
+        '5432109',
+        '4206969'
+      ];
+      let withdrawsubjDateList = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday"
+      ];
+      let withdrawSubjTeacherList = [
+        "Ethan",
+        "Mia",
+        "Lucas",
+        "Charlotte",
+        "Benjamin",
+        "Amelia",
+        "Alexander",
+        "Harper",
+        "Mason",
+        "Evelyn"
+      ];
+      for(let i = 1 ; i <= 10 ; i++){
+        await element(by.id("withdraw-course")).click().then(function(){
+        element(by.id(`WithdrawsubjectCode${i}`)).sendKeys(withdrawsubjectCodeList[i-1]);
+        element(by.id(`WithdrawsubjectName${i}`)).sendKeys(withdrawsubjectnameList[i-1]);
+        element(by.id(`WithdrawsubjectSection${i}`)).sendKeys(withdrawsectionCodesList[i-1]);
+        element(by.id(`WithdrawsubjectDate${i}`)).sendKeys(withdrawsubjDateList[i-1]);
+        element(by.id(`WithdrawsubjectCredit${i}`)).sendKeys("3");
+        element(by.id(`WithdrawsubjectTeacher${i}`)).sendKeys(withdrawSubjTeacherList[i-1]);
       });
+      }
+      
       await element(by.id("submit")).click();
       browser.sleep(2000);
       element(by.css("body > h1:nth-child(1)")).getText().then(function(Text){
